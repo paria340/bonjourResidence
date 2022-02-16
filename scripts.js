4dbb3ad682883b6d9cfade747107e85225bbe313
Parisa Moshfegh <paria340@gmail.com>
2022-02-15 17:49:05 -0800

    modale added

--- scripts.js
+++ scripts.js
@@ -2,14 +2,29 @@
 
 modale = {};
 
-modale.onSubmit = function(){
-    const button = document.getElementById('button')
+const button = document.getElementById('button')
+const popupModale = document.getElementById('popupModale')
+const bodyBlackout = document.getElementById('bodyBlackout')
+
+modale.onClick = function(){
     button.addEventListener('click', function(event){
-        console.log("hello")
+        event.preventDefault();
+        popupModale.classList.add('isVisible')
+        bodyBlackout.style.display='inherit'
+        bodyBlackout.classList.add('bodyBlackout')
+    })
+}
+
+modale.close = function(){
+    const close = document.getElementById('close')
+    close.addEventListener('click', function(event){
+        popupModale.classList.remove('isVisible')
+        bodyBlackout.classList.remove('bodyBlackout')
     })
 }
 
 modale.init = function(){
-    modale.onSubmit()
+    modale.onClick()
+    modale.close()
 }
 modale.init()
